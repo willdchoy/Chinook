94b2ceab9a91dfc010565ac2d425ee355b90f49e
William Choy <williamchoy@Williams-MacBook-Pro.local>
2026-03-26 13:20:39 -0400

    Merge api and db docker files for depends_on.

--- apps/api/docker-destroy.sh
+++ apps/api/docker-destroy.sh
@@ -1,2 +1,4 @@
 docker rm ch-api-web-1
 docker image rm ch-api-web
+docker rm ch-db-1 
+docker image rm postgres
--- apps/db/docker-destroy.sh
+++
@@ -1,3 +0,0 @@
-docker rm ch-db-1 
-docker image rm ch-api-web
-docker volume rm ch_db-data
