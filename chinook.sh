5a1753a237daf1b039046693ee3d007df52a0e99
William Choy <williamchoy@Williams-MacBook-Pro.local>
2026-05-20 18:16:02 -0400

    Refactor chinook.sh.

--- chinook.sh
+++ chinook.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function drawChoppa() {
   echo
 cat << "EOF"
@@ -12,14 +14,14 @@ cat << "EOF"
 ...................................................
 EOF
   echo
-  echo "\033[1;32mChinook - Get to the Choppa! \033[0m"
+  echo -e "\033[1;32mChinook - Get to the Choppa! \033[0m"
   echo          
 }
 
 # core/client --------------------------------------------------------
 
 function createCoreServices() {
-  echo "\033[1;32mChinook - Setting up core services (networking)... \033[0m"
+  echo -e "\033[1;32mChinook - Setting up core services (networking)... \033[0m"
   docker network create ch-web \
     --attachable \
     -d bridge \
@@ -33,13 +35,14 @@ function createCoreServices() {
 }
 
 function createDockerRegistry() {
-  echo "\033[1;32mChinook - Starting docker registry... \033[0m"
-  docker compose -f --remove-orphans ./docker/docker-compose-regi.yml up -d
+  createCoreServices
+  echo -e "\033[1;32mChinook - Starting docker registry... \033[0m"
+  docker compose -v -f ./docker/docker-compose-regi.yml up -d
   echo
 }
 
 function createClientImages() {
-  echo "\033[1;32mChinook - Building and deploy images to docker registry... \033[0m"
+  echo -e "\033[1;32mChinook - Building and deploy images to docker registry... \033[0m"
   make -C ./docker/ch-base docker-build
   make -C ./apps/api docker-build
   make -C ./apps/db docker-build
@@ -47,31 +50,31 @@ function createClientImages() {
 }
 
 function createClientContainers() {
-  echo "\033[1;32mChinook - Composing web containers... \033[0m"
+  echo -e "\033[1;32mChinook - Composing web containers... \033[0m"
   docker compose -f ./docker/docker-compose-web.yml up -d
   echo 
 }
 
 function seedClient() {
-  echo "\033[1;32mChinook - Seeding database... \033[0m"
+  echo -e "\033[1;32mChinook - Seeding database... \033[0m"
   make -C ./apps/db docker-seed
   echo
 }
 
 function destroyClientContainers() {
-  echo "\033[1;32mChinook - Stopping and removing containers... \033[0m"
+  echo -e "\033[1;32mChinook - Stopping and removing containers... \033[0m"
   docker rm -f ch-api-web ch-db-pg ch-regi
   echo
 }
 
 function destroyClientImages() {
-  echo "\033[1;32mChinook - Removing images... \033[0m"
+  echo -e "\033[1;32mChinook - Removing images... \033[0m"
   docker rmi localhost:5000/ch/base:latest localhost:5000/ch/api-web:latest localhost:5000/ch/db-pg:latest
   echo
 }
 
 function destroyCoreServices() {
-  echo "\033[1;32mChinook - Destroying core services... \033[0m"
+  echo -e "\033[1;32mChinook - Destroying core services... \033[0m"
   docker network rm ch-web ch-obsvr
   echo
 }
@@ -79,19 +82,19 @@ function destroyCoreServices() {
 # obsvr --------------------------------------------------------
 
 function createObsvrContainers() {
-  echo "\033[1;32mChinook - Composing Obsvr containers... \033[0m"
+  echo -e "\033[1;32mChinook - Composing Obsvr containers... \033[0m"
   docker compose -f ./docker/docker-compose-obsvr.yml up -d
   echo
 }
 
 function destroyObsvrImages() {
-  echo "\033[1;32mChinook - Removing images... \033[0m"
+  echo -e "\033[1;32mChinook - Removing images... \033[0m"
   docker rmi grafana/grafana:latest grafana/loki:latest prom/prometheus:latest
   echo
 }
 
 function destroyObsvrContainers() {
-  echo "\033[1;32mChinook - Stopping and removing containers... \033[0m"
+  echo -e "\033[1;32mChinook - Stopping and removing containers... \033[0m"
   docker rm -f -v ch-graf ch-loki ch-prom
   echo
 }
@@ -99,7 +102,7 @@ function destroyObsvrContainers() {
 # case handlers --------------------------------------------------------
 
 function createClient() {
-  createCoreServices
+  destroyClientContainers
   createDockerRegistry
   createClientImages
   createClientContainers
@@ -107,22 +110,19 @@ function createClient() {
 }
 
 function createObsvr() {
-  createCoreServices
   destroyObsvrContainers
   createObsvrContainers
 }
 
 function createAll() {
-  drawChoppa
   destroyAll
-  createObsvrContainers
-  createClient  
+  createObsvr
+  createClient
 }
 
 function destroyClient() {
   destroyClientContainers
   destroyClientImages
-  destroyCoreServices
 }
 
 function destroyObsvr() {
@@ -135,28 +135,41 @@ function destroyAll() {
 }
 
 # handle init --------------------------------------------------------
+args=("all, destroy, client, obsvr, regi, setup")
+error_message="\033[1;31mChinook - Argument of < ${args} > is required! \033[0m"
 
-error_message="\033[1;31mChinook - Argument of < all | destroy | client | obsvr > is required \033[0m"
-
-if [ "$#" -eq 0 ]; then
-  echo "${error_message}"
-  exit 1
+if [[ ! $(echo ${args[@]} | fgrep -w $1) ]]; then
+    echo -e ${error_message}
+    exit 1
 fi
+
+drawChoppa
 
 case "$1" in
   all)
+    createCoreServices
     createAll
     ;;
   destroy)
     destroyAll
     ;;
   client)
+    createCoreServices
     createClient
     ;;
   obsvr)
+    createCoreServices
     createObsvr
     ;;
+  regi)
+    createDockerRegistry
+    ;;
+  setup)
+    echo "Run the following commands to add chinook to your PATH and enable autocomplete:"
+    echo "export PATH="\$PATH:$(pwd)""
+    echo "complete -W "all destroy client obsvr regi setup" chinook.sh"
+    ;;
   *)
-    echo "${error_message}"
+    echo -e "${error_message}"
     exit 1
 esac
